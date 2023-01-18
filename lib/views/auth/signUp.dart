4596c4bda78af6bc26edd78ac9e027cd9fa1be3a
2201Hina <hina@rivaaninfotech.com>
2023-01-19 01:00:39 +0530

    19 jan 2023
    otp

--- lib/views/auth/signUp.dart
+++ lib/views/auth/signUp.dart
@@ -1,73 +1,7 @@
-// import 'package:flutter/material.dart';
-//
-//
-// import '../OtpScreen.dart';
-//
-// class SignUp extends StatefulWidget {
-//   @override
-//   _SignUpState createState() => _SignUpState();
-// }
-//
-// class _SignUpState extends State<SignUp> {
-//   TextEditingController _controller = TextEditingController();
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Phone Auth'),
-//       ),
-//       body: Column(
-//         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//         children: [
-//           Column(children: [
-//             Container(
-//               margin: EdgeInsets.only(top: 60),
-//               child: Center(
-//                 child: Text(
-//                   'Phone Authentication',
-//                   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
-//                 ),
-//               ),
-//             ),
-//             Container(
-//               margin: EdgeInsets.only(top: 40, right: 10, left: 10),
-//               child: TextField(
-//                 decoration: InputDecoration(
-//                   hintText: 'Phone Number',
-//                   prefix: Padding(
-//                     padding: EdgeInsets.all(4),
-//                     child: Text('+91'),
-//                   ),
-//                 ),
-//                 maxLength: 10,
-//                 keyboardType: TextInputType.number,
-//                 controller: _controller,
-//               ),
-//             )
-//           ]),
-//           Container(
-//             margin: EdgeInsets.all(10),
-//             width: double.infinity,
-//             child: TextButton(
-//               onPressed: () {
-//                 Navigator.of(context).push(MaterialPageRoute(
-//                     builder: (context) => OTPScreen(_controller.text)));
-//               },
-//               child: Text(
-//                 'Next',
-//                 style: TextStyle(color: Colors.white),
-//               ),
-//             ),
-//           )
-//         ],
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 
-
+import '../custom_widget/custom_text_field.dart';
+import 'otp_verification_screen.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -77,13 +11,12 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
-  TextEditingController countryCode = TextEditingController();
-  var phone="";
+  TextEditingController phone = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
     // TODO: implement initState
-    countryCode.text = "+91";
     super.initState();
   }
 
@@ -94,6 +27,8 @@ class _SignUpState extends State<SignUp> {
         margin: EdgeInsets.only(left: 25, right: 25),
         alignment: Alignment.center,
         child: SingleChildScrollView(
+            child: Form(
+          key: _formKey,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -106,7 +41,7 @@ class _SignUpState extends State<SignUp> {
                 height: 25,
               ),
               Text(
-                "Phone Verification",
+                "Sign Up",
                 style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
               ),
               SizedBox(
@@ -122,47 +57,38 @@ class _SignUpState extends State<SignUp> {
               SizedBox(
                 height: 30,
               ),
-              Container(
-                height: 55,
-                decoration: BoxDecoration(
-                    border: Border.all(width: 1, color: Colors.grey),
-                    borderRadius: BorderRadius.circular(10)),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      width: 10,
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(color: Colors.black, width: 1.5),
+                        borderRadius: BorderRadius.circular(25.0)),
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                    child: Center(
+                      child: Text("+91"),
                     ),
-                    SizedBox(
-                      width: 40,
-                      child: TextField(
-                        controller: countryCode,
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                        ),
-                      ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: CustomTextField(
+                      hintName: "Enter phone no.",
+                      fieldController: phone,
+                      keyboard: TextInputType.phone,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter mobile number ';
+                        } else if (value.length != 10) {
+                          return 'Mobile Number must be of 10 digit';
+                        } else {
+                          return null;
+                        }
+                      },
                     ),
-                    Text(
-                      "|",
-                      style: TextStyle(fontSize: 33, color: Colors.grey),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Expanded(
-                        child: TextField(
-                          onChanged: (value){
-                            phone=value;
-                          },
-                          keyboardType: TextInputType.phone,
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "Phone",
-                          ),
-                        ))
-                  ],
-                ),
+                  ),
+                ],
               ),
               SizedBox(
                 height: 20,
@@ -176,165 +102,22 @@ class _SignUpState extends State<SignUp> {
                         shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(10))),
                     onPressed: () {
-                      // await FirebaseAuth.instance.verifyPhoneNumber(
-                      //   phoneNumber: '${countryCode.text+phone}',
-                      //   verificationCompleted: (PhoneAuthCredential credential) {},
-                      //   verificationFailed: (FirebaseAuthException e) {},
-                      //   codeSent: (String verificationId, int? resendToken) {},
-                      //   codeAutoRetrievalTimeout: (String verificationId) {},
-                      // );
-                      //Navigator.pushNamed(context, 'verify');
-                      //Navigator.of(context).push(MaterialPageRoute(builder: (context) => OTPscreen(number: phone),));
+                      if (_formKey.currentState!.validate()) {
+                        //// first check number register or not api call
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => OtpVerificationScreen(
+                                      phoneNumber: "${phone.text.trim()}",
+                                    )));
+                      }
                     },
                     child: Text("Send the code")),
               )
             ],
           ),
-        ),
+        )),
       ),
     );
   }
-  // TextEditingController _controller = TextEditingController();
-  // @override
-  // bool? check1 = false;
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     backgroundColor: Colors.white,
-  //     body: SingleChildScrollView(
-  //       child: Padding(
-  //         padding: const EdgeInsets.all(30.0),
-  //         child: Form(
-  //           child: Column(
-  //             // crossAxisAlignment: CrossAxisAlignment.center,
-  //             children: <Widget>[
-  //               SizedBox(height: 50),
-  //               Image.asset("img/celebrationstation.png"),
-  //               SizedBox(height: 30),
-  //               RichText(
-  //                 textAlign: TextAlign.center,
-  //                 text:TextSpan(
-  //                     style: TextStyle(
-  //                       fontSize: 30,
-  //                       color: Colors.black,
-  //                       fontWeight: FontWeight.bold,
-  //                       shadows: <Shadow>[
-  //                         Shadow(
-  //                           offset: Offset(1.0, 2.5),
-  //                           blurRadius: 3.0,
-  //                           color: Color.fromARGB(100, 0, 0, 0),
-  //                         ),
-  //                       ],
-  //                     ),
-  //                     children: const <TextSpan>[
-  //                       TextSpan(text: 'Signup for Free',style: TextStyle(letterSpacing: 2.0))
-  //                     ]
-  //                 ),
-  //               ),
-  //               SizedBox(height: 25.0),
-  //               TextFormField(
-  //                 textInputAction: TextInputAction.next,
-  //                 decoration: InputDecoration(
-  //                   enabledBorder: OutlineInputBorder(
-  //                     borderRadius: BorderRadius.circular(20),
-  //                     borderSide: BorderSide(
-  //                       color: Colors.grey,
-  //                       width: 2.0,
-  //                     ),
-  //                   ),
-  //                   focusedBorder: OutlineInputBorder(
-  //                     borderRadius: BorderRadius.circular(20),
-  //                     borderSide: BorderSide(
-  //                       color: Colors.black,
-  //                       width: 2.0,
-  //                     ),
-  //                   ),
-  //                   hintText: 'Enter Mobile No',
-  //                 ),maxLength: 10,keyboardType: TextInputType.number, controller: _controller,
-  //               ),
-  //               SizedBox(height: 15.0),
-  //               TextFormField(
-  //                 textInputAction: TextInputAction.next,
-  //                 obscureText: true,
-  //                 enableSuggestions: false,
-  //                 autocorrect: false,
-  //                 decoration: InputDecoration(
-  //                   enabledBorder: OutlineInputBorder(
-  //                     borderRadius: BorderRadius.circular(20),
-  //                     borderSide: BorderSide(
-  //                       color: Colors.grey,
-  //                       width: 2.0,
-  //                     ),
-  //                   ),
-  //                   focusedBorder: OutlineInputBorder(
-  //                     borderRadius: BorderRadius.circular(20),
-  //                     borderSide: BorderSide(
-  //                       color: Colors.black,
-  //                       width: 2.0,
-  //                     ),
-  //                   ),
-  //                   hintText: 'Enter OTP',
-  //                 ),
-  //               ),
-  //               Row(
-  //                 mainAxisAlignment: MainAxisAlignment.start,
-  //                 children: [
-  //                   SizedBox(
-  //                     width: 30.0,
-  //                     child: Checkbox(
-  //                       value: check1,
-  //                       onChanged: (bool? value) {
-  //                         setState(() {
-  //                           check1 = value;
-  //                         });
-  //                       },
-  //                     ),
-  //                   ),
-  //                   Expanded(
-  //                     child: Container(
-  //                       child: Text('I agree'),
-  //                     ),
-  //                   ),
-  //                   Text('Resend OTP'),
-  //                 ],
-  //               ),
-  //               SizedBox(height: 25.0),
-  //               Center(
-  //                 child: SizedBox(
-  //                     height:50, //height of button
-  //                     width:double.infinity, //width of button
-  //                     child:ElevatedButton(
-  //                       style: ElevatedButton.styleFrom(
-  //                         primary: Colors.lime[200], //background color of button
-  //                         elevation: 3,
-  //                         shape: RoundedRectangleBorder( //to set border radius to button
-  //                             borderRadius: BorderRadius.circular(20)
-  //                         ),
-  //                       ),
-  //                       onPressed: () async{
-  //                         await FirebaseAuth.instance.verifyPhoneNumber(
-  //                           phoneNumber: '+44 7123 123 456',
-  //                           verificationCompleted: (PhoneAuthCredential credential) {},
-  //                           verificationFailed: (FirebaseAuthException e) {},
-  //                           codeSent: (String verificationId, int? resendToken) {},
-  //                           codeAutoRetrievalTimeout: (String verificationId) {},
-  //                         );
-  //                         //Navigator.of(context).push(MaterialPageRoute(builder: (context) => OTPscreen(number: _controller.text),));
-  //                       },
-  //                       child: Text("Signup",
-  //                         style:TextStyle(
-  //                             fontWeight: FontWeight.bold,
-  //                             fontSize: 16.0,
-  //                             color: Colors.black
-  //                         ),
-  //                       ),
-  //                     )
-  //                 ),
-  //               ),
-  //             ],
-  //           ),
-  //         ),
-  //       ),
-  //     ),
-  //   );
-  // }
 }
--- lib/views/auth/splash_screen.dart
+++ lib/views/auth/splash_screen.dart
@@ -1,12 +1,10 @@
 import 'dart:async';
 import 'package:celebration_station_sturcture/views/auth/login_screen.dart';
+import 'package:celebration_station_sturcture/views/auth/signUp.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import '../../dashboard/bottomNavBar/bottom_nav_bar.dart';
-import '../../dashboard/bottomNavBar/tabs/ourServices-1.dart';
 import '../../services/shared_preference.dart';
-import '../home.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -16,50 +14,37 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-
-
-
   @override
-  initState(){
-
-    // TODO: implement initState
+  initState() {
     super.initState();
     getData();
   }
 
-  // Future getData() async{
-  //
-  //   id = await Preferances.getString("id");
-  //   Future.delayed(Duration(seconds: 3)).then((value) async {
-  //     print('$id');
-  //     if(id != null){
-  //       Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) =>
-  //           OurServices()), (Route<dynamic> route) => false);
-  //     }else {
-  //       Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) =>
-  //       const LoginScreen()), (Route<dynamic> route) => false);
-  //     }
-  //   });
-  // }
-  Future getData() async{
+  Future getData() async {
     Future.delayed(Duration(seconds: 3)).then((value) async {
       String? id = await Preferances.getString("id");
       print("userId:=${id}");
-      if(id != null){
-          Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) =>
-              BottomNavBar()), (Route<dynamic> route) => false);
-      }else {
-        Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) =>
-        const LoginScreen()), (Route<dynamic> route) => false);
+      if (id != null) {
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => const BottomNavBar()),
+            (Route<dynamic> route) => false);
+      } else {
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => SignUp()),
+            (Route<dynamic> route) => false);
       }
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Container(
         child: Scaffold(
-          body: Center(child:Image.asset("asset/images/logo.png",scale: 5,)),
-        )
-    );
+      body: Center(
+          child: Image.asset(
+        "asset/images/logo.png",
+        scale: 5,
+      )),
+    ));
   }
 }
